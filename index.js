7989466c741664dc7ac20d21b773693b8474cc80
Almenon <username@users.noreply.github.com>
2016-11-12 19:04:36 -0800

    added more challenges and fixed IE bug

--- index.js
+++ index.js
@@ -6,9 +6,33 @@ var path = require('path');
 var ready = false;
 var id;
 
-var possibilities = ['cat\nbat\nsat\nlat\nrat\nmat\nfat\nzat',
-'123\n1231254215134234234243.\nwhy.\nlol.','dude@gmail.com\nbobalex@yahoo.com duh\nwhat is the flight speed of an african ostrich?']
-var goals = [[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19,21,22,23,25,26,27],[1,2,3,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26],[1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]]
+
+var easy = [{
+	"text": "cat\nbat\nsat\nlat\nrat\nmat\nfat\nzat",
+	"goal": [1,5,9,13,17,21,25]
+},
+{
+	"text": "123\n1231254215134234234243.\nwhy.\nlol.",
+	"goal": [1,2,3,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+},
+{"text":"cat\nbat\nsat\nlat\nrat\nmat\nfat\nzat","goal":[2,3,6,7,10,11,14,15,18,19,22,23,26,27,30,31]},
+{
+	"text": "dudethisisASUPERLONGEMAILOHMYGOD@gmail.com\nwhat is the flight speed of an african ostrich?",
+	"goal": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]
+},
+{"text":"d.de.a.b.c.d.e.f.fa.","goal":[3,4,18,19]},
+{"text":"Some flaGs might come in handy...\nggggggGGgggGGGggggGGGGgggggGGGGGggggg\nGggggGGGGGgggGGGGgggGGGgggGGggggGGg\ngggGGggGgggGGGggGgGGgGgGgGGGGGgGgGg\n","goal":[9,14,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143]},
+];
+var hard = [
+{
+	"text": "dude@gmail.com\nbobalex@yahoo.com duh\nwhat is the flight speed of an african ostrich?",
+	"goal": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]
+},
+{"text":":) :):)a woeifjoewi2-394 :):):) :)\n:):):) :) :):):):) :):) :):):)\n: ) ): :)))","goal":[26,27,28,29,30,31,36,37,38,39,40,41,46,47,48,49,50,51,52,53,60,61,62,63,64,65]},
+{"text":"السلام والمحبة والازدهار.\npeace, love, and prosperity.","goal":[1,2,3,4,5,6,8,9,10,11,12,13,14,16,17,18,19,20,21,22,23,24]},
+{"text":"\\(.^$[{ you will never escape\n you will never escape \\(.^$[{\nyou will \\(.^$[{ never escape","goal":[1,2,3,4,5,6,7,54,55,56,57,58,59,60,71,72,73,74,75,76,77]},
+];
+var allChallenges = easy.concat(hard);
 var index = 0;
 var challenge = {
 	'stringToMatch':'',
@@ -16,9 +40,9 @@ var challenge = {
 	'id':0,
 }
 function randomChallenge(){
-	index = Math.floor(Math.random()*goals.length);
-	challenge.stringToMatch = possibilities[index];
-	challenge.goal = goals[index];
+	index = Math.floor(Math.random()*allChallenges.length);
+	challenge.stringToMatch = allChallenges[index]['text'];
+	challenge.goal = allChallenges[index]['goal'];
 	challenge.id = index;
 	return challenge;
 }
@@ -33,7 +57,7 @@ function range(start, count) {
 
 function iswin(matches,challengeId){
 		var i = 0;
-		var goal = goals[challengeId];
+		var goal = allChallenges[index]['goal'];
 		if(matches == null || matches.length == 0 || matches.length == 1 && matches[0] == '') return false;
 		charsMatch = matches.every(function(match){
 			nums = range(match.index+1,match[0].length) // add 1 because goal uses 1-based indexing
@@ -53,7 +77,7 @@ function applyRegex(regexString,challengeId){
 	}
 	catch(SyntaxError){ return; }
 	matches = [];
-	stringToMatch = possibilities[challengeId];
+	stringToMatch = allChallenges[challengeId]['text'];
 
 	if(re.flags.search('g') > -1){
 		while((match = re.exec(stringToMatch)) != null && match != ''){
--- public/js/practice.js
+++ public/js/practice.js
@@ -1,20 +1,30 @@
 
-var challenges = [{
+var easy = [{
 	"text": "cat\nbat\nsat\nlat\nrat\nmat\nfat\nzat",
-	"goal": [1,2,3,5,6,7,9,10,11,13,14,15,17,18,19,21,22,23,25,26,27]
+	"goal": [1,5,9,13,17,21,25]
 },
 {
 	"text": "123\n1231254215134234234243.\nwhy.\nlol.",
 	"goal": [1,2,3,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
 },
+{"text":"cat\nbat\nsat\nlat\nrat\nmat\nfat\nzat","goal":[2,3,6,7,10,11,14,15,18,19,22,23,26,27,30,31]},
+{
+	"text": "dudethisisASUPERLONGEMAILOHMYGOD@gmail.com\nwhat is the flight speed of an african ostrich?",
+	"goal": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]
+},
+{"text":"d.de.a.b.c.d.e.f.fa.","goal":[3,4,18,19]},
+{"text":"Some flaGs might come in handy...\nggggggGGgggGGGggggGGGGgggggGGGGGggggg\nGggggGGGGGgggGGGGgggGGGgggGGggggGGg\ngggGGggGgggGGGggGgGGgGgGgGGGGGgGgGg\n","goal":[9,14,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143]},
+];
+var hard = [
 {
 	"text": "dude@gmail.com\nbobalex@yahoo.com duh\nwhat is the flight speed of an african ostrich?",
 	"goal": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]
 },
-{"text":"cat\nbat\nsat\nlat\nrat\nmat\nfat\nzat","goal":[2,3,6,7,10,11,14,15,18,19,22,23,26,27,30,31]},
-{"text":"d.de.a.b.c.d.e.f.fa.","goal":[3,4,18,19]},
-{"text":":) :):)a woeifjoewi2-394 :):):) :)\n:):):) :) :):):):) :):) :):):)\n: ) ): :)))","goal":[26,27,28,29,30,31,36,37,38,39,40,41,46,47,48,49,50,51,52,53,60,61,62,63,64,65]}
+{"text":":) :):)a woeifjoewi2-394 :):):) :)\n:):):) :) :):):):) :):) :):):)\n: ) ): :)))","goal":[26,27,28,29,30,31,36,37,38,39,40,41,46,47,48,49,50,51,52,53,60,61,62,63,64,65]},
+{"text":"السلام والمحبة والازدهار.\npeace, love, and prosperity.","goal":[1,2,3,4,5,6,8,9,10,11,12,13,14,16,17,18,19,20,21,22,23,24]},
+{"text":"\\(.^$[{ you will never escape\n you will never escape \\(.^$[{\nyou will \\(.^$[{ never escape","goal":[1,2,3,4,5,6,7,54,55,56,57,58,59,60,71,72,73,74,75,76,77]},
 ];
+var challenges = hard.concat(easy);
 var index = Math.floor(Math.random()*challenges.length)
 var stringToMatch = challenges[index].text
 var goal = challenges[index].goal
@@ -71,7 +81,7 @@ function highlightRegExp(re,element){
 	$(element + ' span').removeClass('highlight');
 	matches = [];
 
-	if(re.flags.search('g') > -1){
+	if(flags.search('g') > -1){ // when IE supports re.flags use that instead
 		while((match = re.exec(stringToMatch)) != null && match != ''){
 			highlightMatch(match,element);
 			matches.push(match);
